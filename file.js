bf823b0767c28c20828612d1bf8b91c8c528e023
shivam agarwal <shivamagarwaloff@gmail.com>
2022-03-05 21:28:09 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "850px";
+    document.getElementById("main").style.marginLeft = "850px";
 }
 
 function closeNav() {
