268620071bfd3149d3d2d235ac55acffeef987e5
shivam agarwal <shivamagarwaloff@gmail.com>
2022-03-06 11:44:48 +0530

    update

--- file.js
+++ file.js
@@ -14,3 +14,13 @@ function DarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function mic() {
+    document.getElementById('inpa').src = 'voice.gif';
+    let reco = new webkitSpeechRecognition();
+    reco.lang = "en-GB";
+    reco.onresult = function(event) {
+        console.log(event);
+        document.getElementById('inp').value = event.results[0][0].transcript;
+    }
+}
